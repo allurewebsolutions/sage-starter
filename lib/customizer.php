3102eea46d28d6547338ec21d9fca2520a1c9f90
Mike Doubintchik <mike@allurewebsolutions.com>
2016-09-07 14:02:10 -0400

    Add jumbotron to customizer

--- lib/customizer.php
+++ lib/customizer.php
@@ -15,38 +15,70 @@ function customize_register($wp_customize)
   /**
    * Add Custom Theme Options
    */
-  $wp_customize->add_panel( 'theme_options', array(
+  $wp_customize->add_panel('theme_options', array(
     'priority' => 10,
     'capability' => 'edit_theme_options',
     'theme_supports' => '',
-    'title' => __( 'Theme Options', 'textdomain' ),
-    'description' => __( 'Theme specific options', 'textdomain' ),
-  ) );
+    'title' => __('Theme Options', 'textdomain'),
+    'description' => __('Theme specific options', 'textdomain'),
+  ));
 
-  $wp_customize->add_section( 'footer', array(
+  /**
+   * Inner Page Banner
+   */
+  $wp_customize->add_section('jumbotron', array(
     'priority' => 10,
     'capability' => 'edit_theme_options',
     'theme_supports' => '',
-    'title' => __( 'Footer', 'textdomain' ),
+    'title' => __('Jumbotron', 'textdomain'),
+    'description' => 'The is the jumbotron that appears below the header and above the page content. It container the page title and an optional tagline.',
+    'panel' => 'theme_options',
+  ));
+
+  $wp_customize->add_setting('jumbotron-tagline', array(
+    //'default' => 'Welcome to ' . get_bloginfo('name'),
+    'type' => 'theme_mod',
+    'capability' => 'edit_theme_options',
+    'transport' => '',
+    //'sanitize_callback' => 'esc_textarea',
+  ));
+
+  $wp_customize->add_control('jumbotron-tagline', array(
+    'type' => 'textarea',
+    'priority' => 10,
+    'section' => 'jumbotron',
+    'label' => __('Jumbotron Tagline', 'textdomain'),
+    'description' => 'You can use HTML here',
+  ));
+
+
+  /**
+   * Footer Section
+   */
+  $wp_customize->add_section('footer', array(
+    'priority' => 10,
+    'capability' => 'edit_theme_options',
+    'theme_supports' => '',
+    'title' => __('Footer', 'textdomain'),
     'description' => '',
     'panel' => 'theme_options',
-  ) );
+  ));
 
-  $wp_customize->add_setting( 'copyright', array(
+  $wp_customize->add_setting('copyright', array(
     'default' => 'Copyright ' . current_time('Y') . ' &copy; ' . get_bloginfo('name') . '. All Rights Reserved.',
     'type' => 'theme_mod',
     'capability' => 'edit_theme_options',
     'transport' => '',
     //'sanitize_callback' => 'esc_textarea',
-  ) );
+  ));
 
-  $wp_customize->add_control( 'copyright', array(
+  $wp_customize->add_control('copyright', array(
     'type' => 'textarea',
     'priority' => 10,
     'section' => 'footer',
-    'label' => __( 'Footer Copyright', 'textdomain' ),
+    'label' => __('Footer Copyright', 'textdomain'),
     'description' => 'You can use HTML here',
-  ) );
+  ));
 }
 
 add_action('customize_register', __NAMESPACE__ . '\\customize_register');
--- templates/page-header.php
+++ templates/page-header.php
@@ -3,6 +3,8 @@
 <div class="jumbotron">
   <div class="container">
     <h1><?= Roots\Sage\Titles\title(); ?></h1>
-    <h3>This is a description coming from customizer</h3>
+    <?php if (get_theme_mod('jumbotron-tagline') != '') : ?>
+      <h4><?php echo get_theme_mod('jumbotron-tagline'); ?></h4>
+    <?php endif; ?>
   </div>
 </div>
