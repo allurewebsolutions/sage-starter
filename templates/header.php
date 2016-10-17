63cc412933f0cc5dc5a4870e73e302ce1b64dbdc
Mike Doubintchik <mike@allurewebsolutions.com>
2016-10-17 12:39:14 -0400

    move breadcrumbs to page-header

--- templates/header.php
+++ templates/header.php
@@ -53,8 +53,3 @@
   </header><!-- /.container-fluid -->
 
 <?php } ?>
-
-<?php if (function_exists('bcn_display') && !is_front_page() && !get_theme_mod('hide-breadcrumbs')) {
-  /** breadcrumbs **/
-  get_template_part('templates/components/breadcrumbs');
-} ?>
